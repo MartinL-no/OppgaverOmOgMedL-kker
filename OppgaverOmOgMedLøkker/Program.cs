bbb856771effb3e0a4b783545223c0cc3a47ef49
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-15 10:23:47 +0100

    initialise project

---
+++ OppgaverOmOgMedLøkker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OppgaverOmOgMedLøkker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
