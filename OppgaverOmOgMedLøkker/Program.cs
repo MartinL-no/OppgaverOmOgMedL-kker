d45ec63a34b36a06243b7ae2d391a25276e27573
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-15 10:39:19 +0100

    oppgave-2

--- OppgaverOmOgMedLøkker/Program.cs
+++ OppgaverOmOgMedLøkker/Program.cs
@@ -6,13 +6,24 @@ namespace OppgaverOmOgMedLøkker
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Oppgave1:");
             Oppgave1();
+
+            Console.WriteLine("Oppgave2:");
+            Oppgave2("Terje er kul");
         }
         static void Oppgave1()
         {
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine("Terje er kul");
+            }
+        }
+        static void Oppgave2(string input)
+        {
+            foreach (var character in input)
+            {
+                Console.WriteLine(character);
             }
         }
     }
